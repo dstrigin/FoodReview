6c2ad252f022ff6bd8884686aaa73c9c128a0bc4
iwhanter <iw.androsow2014@yandex.ru>
2024-12-13 00:47:45 +0300

    Добавил регистрацию пользователя
    После регистрации на почту приходит письмо, где есть ссылка для подтверждение аккаунта. После этого перебрасывает сразу на страницу входа

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ group :development, :test do
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
   gem "rubocop-rails-omakase", require: false
 end
+gem "devise"
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,18 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com', # Или ваш домен
+    user_name:            'freviews085@gmail.com', # Ваш email
+    password:             'pmhw feni vqbs evib', # Пароль приложения
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
