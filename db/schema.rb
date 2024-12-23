bb64596dfc5ba87be02620c334996bf653b94e9c
dstrigin <dmitriystrigin04@gmail.com>
2024-12-23 09:07:05 +0300

    add photos by asset name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_22_164746) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_23_060014) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_22_164746) do
     t.datetime "updated_at", null: false
     t.integer "type_id"
     t.integer "category_id"
+    t.string "pseudonym"
     t.index ["category_id"], name: "index_places_on_category_id"
     t.index ["type_id"], name: "index_places_on_type_id"
   end
@@ -93,7 +94,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_22_164746) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
   add_foreign_key "reviews", "places"
   add_foreign_key "reviews", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,8 @@ Place.create(
     address: "Пушкинская, 213/106",
     description: "Пожалуй, самый известный ресторан азиатской кухни в городе. Вы будете без ума от этого том яма.",
     type_id: Type.find_by(name: "Ресторан")&.id,
-    category_id: Category.find_by(name: "Азиатская кухня")&.id
+    category_id: Category.find_by(name: "Азиатская кухня")&.id,
+    pseudonym: "Pad_Thai"
   }
 )
 
@@ -22,7 +23,8 @@ Place.create(
     address: "Коммунистический, 36/4 (парк Сказка)",
     description: "Уютная кофейня с большим ассортиментом. Вы точно найдете, чем согреться",
     type_id: Type.find_by(name: "Кофейня")&.id,
-    category_id: Category.find_by(name: "Кофе")&.id
+    category_id: Category.find_by(name: "Кофе")&.id,
+    pseudonym: "Keks"
   }
 )
 
@@ -32,6 +34,7 @@ Place.create(
     address: "Соборный 17/53",
     description: "Восхваленная многими ростовчанами шаурма. Не оставайтесь в стороне и попробуйте",
     type_id: Type.find_by(name: "Кафе")&.id,
-    category_id: Category.find_by(name: "Стритфуд")&.id
+    category_id: Category.find_by(name: "Стритфуд")&.id,
+    pseudonym: "Kikchak"
   }
 )
